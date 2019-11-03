1d1e542122428e9afd477c1e623d563fa79f2163
Pedro Scaff <pedro@scaff.me>
2019-11-03 19:27:07 +0100

    Use serde default to deserialize missing fields

--- src/config.rs
+++ src/config.rs
@@ -8,24 +8,26 @@ lazy_static! {
     pub static ref EMPTY_ALIASES_MAP: Map<String, String> = Map::new();
 }
 
+#[serde(default)]
 #[derive(Deserialize)]
-struct TomlDeserializableConfig {
-    icons: Option<Map<String, char>>,
-    aliases: Option<Map<String, String>>,
-}
-
 pub struct Config {
     pub icons: Map<String, char>,
     pub aliases: Map<String, String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            icons: super::icons::NONE.clone(),
+            aliases: EMPTY_ALIASES_MAP.clone(),
+        }
+    }
 }
 
 pub fn read_toml_config(filename: &str) -> Result<Config, Error> {
     let mut file = File::open(filename)?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
-    let config: TomlDeserializableConfig = toml::from_str(&buffer)?;
-    Ok(Config {
-        icons: config.icons.unwrap_or(super::icons::NONE.clone()),
-        aliases: config.aliases.unwrap_or(EMPTY_ALIASES_MAP.clone()),
-    })
+    let config: Config = toml::from_str(&buffer)?;
+    Ok(config)
 }
